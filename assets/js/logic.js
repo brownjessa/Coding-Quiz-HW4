be0b57dd412bd4a49a142b64cb083f1bdbefef06
Jessamine Brown <brownjessa11@gmail.com>
2020-08-24 13:45:38 -0500

    declare variables

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+// Declare Variables
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");

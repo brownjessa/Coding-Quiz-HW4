22eb751e62c356c3f3772958485e0b51f242e1c4
Jessamine Brown <brownjessa11@gmail.com>
2020-08-24 13:47:35 -0500

    start quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,3 +10,12 @@ var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
+
+function startQuiz() {
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+  questionsEl.removeAttribute("class");
+  timerId = setInterval(clockTick, 1000);
+  timerEl.textContent = time;
+  getQuestion();
+}
